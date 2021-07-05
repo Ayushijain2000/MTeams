c8f1d2b6513e86ccfb8a405854b06db462076355
Ayushi <ayushijain2738@gmail.com>
2021-07-05 11:25:40 +0530

    Fixing Bugs

--- public/js/dash.js
+++ public/js/dash.js
@@ -1,23 +1,22 @@
 $(".meeting_info").click(function(){
-    $("#dashBoard").css({display:"flex"}), $(".call_container").css({width:"55%"}) , $(".messenger_container").css({width: "25%"}), $(".notes_cont").css({display:"none"}),  $(".watch_cont").css({position:"absolute" , top: "28.2%" , left: "27%"});; 
+    $("#dashBoard").css({display:"flex"}), $(".call_container").css({width:"55%"}) , $(".messenger_container").css({width: "25%"}), $(".notes_cont").css({display:"none"}),  $(".watch_cont").css({position:"absolute" , top: "10%" , left: "40%"}),
+    $(".emoji_holder").css({position:"absolute" , bottom: "12%" , right: "20%"});
  });
 
  $("#hide_meeting_info").click(function(){
     $("#dashBoard").css({display:"none"}), $(".call_container").css({width:"75%"}) , $(".messenger_container").css({width: "25%"}) ,$(".notes_cont").css({display: "none"});
-    $(".watch_cont").css({position:"absolute" , top: "28.2%" , left: "6%"});
+    $(".watch_cont").css({position:"absolute" , top: "10%" , left: "17%"}),
+    $(".emoji_holder").css({position:"absolute" , bottom: "12%" , right: "35%"});
  });
 
  $(".notes_btn").click(function(){
-   $(".notes_cont").css({display: "flex"}) ,$("#dashBoard").css({display:"none"}) , $(".call_container").css({width:"55%"}) , $(".messenger_container").css({width: "25%"}),  $(".watch_cont").css({position:"absolute" , top: "28.2%" , left: "27%"});;
+   $(".notes_cont").css({display: "flex"}) ,$("#dashBoard").css({display:"none"}) , $(".call_container").css({width:"55%"}) , $(".messenger_container").css({width: "25%"}),  $(".watch_cont").css({position:"absolute" , top: "10%" , left: "40%"}),
+   $(".emoji_holder").css({position:"absolute" , bottom: "12%" , right: "20%"});
  })
 
- $("#reactionBtn").mouseover(function(){
+ $("#reactionBtn").click(function(){
   $(".emoji_holder").css({display: "flex"});
  });
-
- $("#reactionBtn").mouseout(function(){
-   $(".emoji_holder").css({display: "none"});
-  });
 
 $(".emoji_holder").mouseover(function(){
    $(".emoji_holder").css({display: "flex"});
